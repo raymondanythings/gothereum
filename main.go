fb92847886390a0ead93e8df375adc2f506e41fd
Raymond <akdfhr2@gmail.com>
2024-01-24 23:06:43 +0900

    [230124] cli flagset

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
 import (
-	"github.com/raymondanythings/gothereum/rest"
+	"flag"
+	"fmt"
+	"os"
 )
 
+func usage() {
+	fmt.Println("Welcome to Blockchain Service")
+	fmt.Println()
+	fmt.Println("Please use the following commands:")
+	fmt.Println()
+	fmt.Println("explorer:	Start the HTML Explorer")
+	fmt.Println("rest:		Start the REST API (recommanded)")
+	os.Exit(0)
+}
+
 func main() {
-	// go explorer.Start(3000)
-	rest.Start(4000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	rest := flag.NewFlagSet("rest", flag.ExitOnError)
+	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+
+	switch os.Args[1] {
+	case "explorer":
+		fmt.Println("Start Explorer")
+	case "rest":
+		rest.Parse(os.Args[2:])
+		fmt.Println("Start REST API")
+	default:
+		usage()
+	}
+
+	if rest.Parsed() {
+		fmt.Println(fmt.Sprint(`Start server `, *portFlag))
+	}
 }
